3c08aba25a6fe388e9274cef8d65bea545e2b1cd
sajidur319370 <sajidurrahmansahib@gmail.com>
2022-05-13 19:48:28 +0600

    gadget master server database created

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rcxdj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("gadgetMaster").collection("product");
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("My Gadget Master Server is running.......");
+});
+
+app.listen(port, () => {
+  console.log("Server is running at", port);
+});
