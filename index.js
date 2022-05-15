b0383012adf8ed4a4c68a35c6e76d4ae607b2b8d
sajidur319370 <sajidurrahmansahib@gmail.com>
2022-05-15 12:41:41 +0600

    single product loaded in inventory/id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -25,6 +25,14 @@ async function run() {
       const cursor = serviceCollection.find(query);
       const products = await cursor.toArray();
       res.send(products);
+    });
+
+    // =================Single product load=============
+    app.get("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await serviceCollection.findOne(query);
+      res.send(product);
     });
   } finally {
   }
