9ca620bb03d6bc8b855c1278089854c94a90bd8e
sajidur319370 <sajidurrahmansahib@gmail.com>
2022-05-15 14:31:07 +0600

    Add item in Inventory

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { response } = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -20,19 +21,25 @@ async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db("gadgetMaster").collection("product");
-    app.get("/product", async (req, res) => {
+    app.get("/inventory", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const products = await cursor.toArray();
       res.send(products);
     });
 
-    // =================Single product load=============
+    // =================get single product=============
     app.get("/inventory/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const product = await serviceCollection.findOne(query);
       res.send(product);
+    });
+    // =================post single product=============
+    app.post("/inventory", async (req, res) => {
+      const newProduct = req.body;
+      const result = await serviceCollection.insertOne(newProduct);
+      res.send(result);
     });
   } finally {
   }
